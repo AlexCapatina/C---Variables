6069284a71c36b5c13f5f17d726b134ae47545cc
Alex <oravetzal@yahoo.com>
2023-03-09 10:53:50 -0500

    How to use numerical system

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main(){       
+    int num1 = 15; //Decimal
+    int num2 = 017; //Octal
+    int num3 = 0x0F; //Hex
+    int num4 = 0b00001111; //Binary
+
+    std::cout << "Number 1: " << num1 << std::endl;
+    std::cout << "Number 2: " << num2 << std::endl;
+    std::cout << "Number 3: " << num3 << std::endl;
+    std::cout << "Number 4: " << num4 << std::endl;
 
     return 0;
 }
