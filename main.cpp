a4e115d32c7425d5a351bbcea63e951990af8e26
Alex <oravetzal@yahoo.com>
2023-03-09 13:35:25 -0500

    Added Integer Modifiers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(){       
+    /*
     int num1 = 15; //Decimal
     int num2 = 017; //Octal
     int num3 = 0x0F; //Hex
@@ -53,6 +54,73 @@ int main(){
     //Check the size of memory taken with sizeof
     std::cout << "sizeof int: " << sizeof(int) << std::endl;
     std::cout << "sizeof truck_count: " << sizeof(truck_count) << std::endl;
+    */
+
+
+//========================================================//
+
+/*
+   //Integer modifiers
+   signed int value1 {10};
+   signed int value2 {-300};
+
+   std::cout << "Value 1: " << value1 << std::endl;
+   std::cout << "Value 2: " << value2 << std::endl;
+   std::cout << "sizeof value 1: " << sizeof(value1) << std::endl;
+   std::cout << "sizeof value 2: " << sizeof(value2) << std::endl;
+
+   unsigned int value3 {4};
+   //unsigned int value4 {-5};//Compiler error
+   short short_var {-32768};
+   short int short_int {405};
+   signed short signed_short {122};
+   signed short int signed_short_int {-456};
+   unsigned short int unsigned_short_int {456};
+
+   int int_var {55};
+   signed signed_var {66};
+   signed int signed_int{77};
+   unsigned int unsigned_int {77};
+
+   long long_var{88};
+   long int long_int {33};
+   signed long signed_long {44};
+   signed long int signed_long_int {44};
+   unsigned long int unsigned_long_int {44};
+
+   long long long_long {888};
+   long long int long_long_int {999};
+   signed long long signed_long_long {444};
+   signed long long int signed_long_long_int {1234};
+   unsigned long long int unsigned_long_long_int {1234};
+
+    std::cout << "Short variable: " << short_var << ", size: " << sizeof (short) << " bytes" << std::endl;
+    std::cout << "Short Int: " << short_int << ", size: " << sizeof (short int) << " bytes" << std::endl;
+    std::cout << "Signed short: " << signed_short << ", size: " << sizeof (signed short) << " bytes" << std::endl;
+    std::cout << "Signed short int: " << signed_short_int << ", size: " << sizeof (signed short int) << " bytes" << std::endl;
+    std::cout << "Unsigned short int: " << unsigned_short_int << ", size: " << sizeof (unsigned short int) << " bytes" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    std::cout << "Int variable: " << int_var << ", size: " << sizeof (int) << " bytes" << std::endl;
+    std::cout << "Signed variable: " << signed_var << ", size: " << sizeof (signed) << " bytes" << std::endl;
+    std::cout << "Signed int: " << signed_int << ", size: " << sizeof (signed int) << " bytes" << std::endl;    
+    std::cout << "Unsigned int: " << unsigned_int << ", size: " << sizeof (unsigned int) << " bytes" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    std::cout << "Long variable: " << long_var << ", size: " << sizeof (long) << " bytes" << std::endl;
+    std::cout << "Long Int: " << long_int << ", size: " << sizeof (long int) << " bytes" << std::endl;
+    std::cout << "Signed Long: " << signed_long << ", size: " << sizeof (signed long) << " bytes" << std::endl;
+    std::cout << "Signed Long int: " << signed_long_int << ", size: " << sizeof (signed long int) << " bytes" << std::endl;
+    std::cout << "Unsigned Long int: " << unsigned_long_int << ", size: " << sizeof (unsigned long int) << " bytes" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    std::cout << "Long Long variable: " << long_long << ", size: " << sizeof (long long) << " bytes" << std::endl;
+    std::cout << "Long Long Int: " << long_long_int << ", size: " << sizeof (long long int) << " bytes" << std::endl;
+    std::cout << "Signed Long Long: " << signed_long_long << ", size: " << sizeof (signed long long) << " bytes" << std::endl;
+    std::cout << "Signed Long Long int: " << signed_long_long_int << ", size: " << sizeof (signed long long int) << " bytes" << std::endl;
+    std::cout << "Unsigned Long Long int: " << unsigned_long_long_int << ", size: " << sizeof (unsigned long long int) << " bytes" << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+    */
 
     return 0;
 }
